e8992b1b3d871f8dd6a4ed0bccbbe3adcf6c30e6
bengus101 <bgustafson29@yahoo.com>
2016-05-16 16:43:36 -0700

    Bringing in changes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	@user = User.authenticate user_params[:email], user_params[:password]
+
+  	if @user
+  		session[:user_id] = @user.id
+  		flash[:success] = "Successfully logged in"
+  		redirect_to root_path
+  	else
+  		flash[:danger] = "Credentials not valid"
+  		redirect_to login_path
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	flash[:success] = "Successfully logged out"
+  	redirect_to root_path
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :password)
+  end
+end
