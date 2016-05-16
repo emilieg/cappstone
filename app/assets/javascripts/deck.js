caa485a0950aaedff337321080014c6abaa04039
Paul <pacalabre@gmail.com>
2016-05-16 14:24:54 -0700

    making sure up to date before pull request

--- app/assets/javascripts/deck.js
+++ app/assets/javascripts/deck.js
@@ -2,6 +2,17 @@
 
 console.log("hi");
 
- $(document).ready(function () {
-        $('body').sectionScroll(); // Easy Peasy Lemon Squeezy
-    })
+ $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+
+ $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      ready: function() { alert('Ready'); }, // Callback for Modal open
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
