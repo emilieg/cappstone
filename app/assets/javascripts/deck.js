c9e8cfd3e485f4f0dc0195f68e705fb892ce790a
Paul <pacalabre@gmail.com>
2016-05-16 19:36:49 -0700

    fixed issues with header shooting up to the events div

---
+++ app/assets/javascripts/deck.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//     $('#try-this-div').sectionScroll();
+//     $('.modal-trigger').leanModal();
+//     $('.modal-trigger2').leanModal();
+//   });
+
+
--- app/assets/javascripts/zdeck.js
+++
@@ -1,9 +0,0 @@
-
-// $(document).ready(function(){
-//   $('body').sectionScroll();
-//   $('.modal-trigger').leanModal();
-//   $('.modal-trigger2').leanModal();
-
-// });
-
-
