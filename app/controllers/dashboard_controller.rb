84f9e9e5d697d262dae8f6e7363afb5f9105c1e9
emilieg <emilie.gerber@hotmail.com>
2016-05-17 12:41:21 -0700

    edit dash and deck controllers with db calls and configured dashboard show view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,21 @@
 class DashboardController < ApplicationController
+  #dashboard will show a list of jobs under that user
+  #future: remove old jobs from this list (make inactive?)
+  # before_action :is_authenticated?
+  
   def show
+    @jobs = Job.all
+    @job = Job.where(user_id: 2) 
+    @company = Company.all
   end
+
+
+  private
+  def job_params
+    params.require(:job).permit(:job_position,:company_name, :user_id)
+  end
+
+
+
+
 end
