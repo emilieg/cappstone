037e62390995019e3aab42b3a3eaaa523dcb5f40
drewrudebusch <drewrudebusch@gmail.com>
2016-05-19 16:50:04 -0700

    Updated resume model to fix download issues

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,3 +1,8 @@
 class Resume < ActiveRecord::Base
 	has_many :job
+
+  mount_uploader :attachment, AttachmentUploader
+  validates :name, presenece: true
+  validates :name, presence: true
+  has_many :job
 end
