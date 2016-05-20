897dd6d0512820fe7fa05578e184658bf9057eaa
drewrudebusch <drewrudebusch@gmail.com>
2016-05-19 17:17:36 -0700

    More fun with resumes

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,5 +1,5 @@
 class Resume < ActiveRecord::Base
-  # mount_uploader :attachment, AttachmentUploader
-  # validates :name, presence: true
+  mount_uploader :attachment, AttachmentUploader
+  validates :name, presence: true
   has_many :job
 end
